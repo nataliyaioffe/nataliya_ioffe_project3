31b7de9c4d3aba8326f6a758f1550a990b8b1346
Nataliya Ioffe <nxioffe@gmail.com>
2018-11-18 13:14:17 -0500

    added more images, need to add ALT text, optimize images, adjust layout

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,40 +10,40 @@ app.data = {
         "<p>\“Our greatest glory is not in never falling, but in rising every time we fall.”\<span>Confucius</span></p>",
         "<p>\“It does not matter how slowly you go as long as you do not stop.”\<span>Confucius</span></p>",
         "<p>\"The true miracle is not walking on water or walking in air, but simply walking on this earth.\" <span>Thích Nhất Hạnh</span></p>",
-        // "<p>\"One must be deeply aware of the impermanence of the world.\"<span>Dogen</span></p>",
-        // "<p>\"Have good trust in yourself… not in the One that you think you should be, but in the One that you are.\"<span>Maezumi Roshi</span></p>",
-        // "<p>\"Who you are is always right.\"<span>Ming-Dao Deng</span></p>",
-        // "<p>\"The search for happiness is one of the chief sources of unhappiness.\"<span>Eric Hoffer</span></p>",
-        // "<p>\"Everything that happens to you is a form of instruction if you pay attention.\"<span>Robert Greene</span></p>",
-        // "<p>\"Relax. Nothing’s missing.\"<span>Maxime Lagacé</span></p>",
-        // "<p>\"Life is a balance of holding on and letting go.\"<span>Rumi</span></p>",
-        // "<p>\"Wherever you are, be there totally.\"<span>Eckhart Tolle</span></p>",
-        // "<p>\"One loses joy and happiness in the attempt to possess them.\"<span>Masanobu Fukuoka</span></p>",
-        // "<p>\"Muddy water is best cleared by leaving it alone.\"<span>Alan Watts</span></p>",
-        // "<p>\"When you realize nothing is lacking, the whole world belongs to you.\"<span>Lao Tzu</span></p>",
-        // "<p>\"Zen is a path of liberation. It liberates you. It is freedom from the first step to the last. You are not required to follow any rules; you are required to find out your own rules and your own life in the light of awareness.\"<span>Osho</span></p>",
-        // "<p>\"Treat every moment as your last. It is not preparation for something else.\"<span>Shunryu Suzuki</span></p>",
-        // "<p>\"Nature does not hurry, yet everything is accomplished.\"<span>Lao Tzu</span></p>",
-        // "<p>\"Be kind whenever possible. It is always possible.\"<span>Dalai Lama</span></p>",
-        // "<p>\"Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.\"<span>Buddha</span></p>",
-        // "<p>\"To seek is to suffer. To seek nothing is bliss.\"<span>Bodhidharma</span></p>",
-        // "<p>\"All things are difficult before they are easy.\"<span>Thomas Fuller</span></p>",
-        // "<p>\"Act without expectation.\"<span>Lao Tzu</span></p>",
-        // "<p>\"Roads were made for journeys not destinations.\"<span>Confucius</span></p>",
-        // "<p>\"Sleep is the best meditation.\"<span>Dalai Lama</span></p>",
-        // "<p>\"The personal life deeply lived always expands into truths beyond itself.\"<span>Anais Nin</span></p>",,
-        // "<p>\"It is easy to believe we are each waves and forget we are also the ocean.\"<span>Jon J. Muth</span></p>",
-        // "<p>\"In the midst of chaos, there is also opportunity.\"<span>Sun Tzu</span></p>",
-        // "<p>\"You look at where you’re going and where you are and it never makes sense, but then you look back at where you’ve been and a pattern seems to emerge.\" <span>Robert M. Pirsig</span></p>",
-        // "<p>\"There is no need for temples; no need for complicated philosophy. Our own brain, our own heart is our temple; the philosophy is kindness.\"<span>Dalai Lama</span></p>",
-        // "<p>\"As a bee gathering nectar does not harm or disturb the color & fragrance of the flower; so do the wise move through the world.\" <span>Buddha</span></p>",
-        // "<p>\"The whole moon and the entire sky are reflected in one dewdrop on the grass.\"<span> Dogen</span></p>",
-        // "<p>\"If your compassion does not include yourself, it is incomplete.\"<span>Buddha</span></p>",
-        // "<p>\"They call it ‘peace of mind’ but maybe it should be called ‘peace from mind.\"<span>Naval Ravikant</span></p>",
-        // "<p>\"If you think you are too small to make a difference, try sleeping with a mosquito.\" - Dalai Lama</p>",
-        // "<p>\"A man is great not because he hasn’t failed; a man is great because failure hasn’t stopped him.\" <span>Confucius</span></p>",
-        // "<p>\"Unhappiness and rain just drift by and are gone, they are visitors.\" <span>John Aske</span></p>",
-        // "<p>\"When you catch yourself slipping into a pool of negativity, notice how it derives from nothing other than resistance to the current situation.\" <span>Donna Quesada</span></p>"   
+        "<p>\"One must be deeply aware of the impermanence of the world.\"<span>Dogen</span></p>",
+        "<p>\"Have good trust in yourself… not in the One that you think you should be, but in the One that you are.\"<span>Maezumi Roshi</span></p>",
+        "<p>\"Who you are is always right.\"<span>Ming-Dao Deng</span></p>",
+        "<p>\"The search for happiness is one of the chief sources of unhappiness.\"<span>Eric Hoffer</span></p>",
+        "<p>\"Everything that happens to you is a form of instruction if you pay attention.\"<span>Robert Greene</span></p>",
+        "<p>\"Relax. Nothing’s missing.\"<span>Maxime Lagacé</span></p>",
+        "<p>\"Life is a balance of holding on and letting go.\"<span>Rumi</span></p>",
+        "<p>\"Wherever you are, be there totally.\"<span>Eckhart Tolle</span></p>",
+        "<p>\"One loses joy and happiness in the attempt to possess them.\"<span>Masanobu Fukuoka</span></p>",
+        "<p>\"Muddy water is best cleared by leaving it alone.\"<span>Alan Watts</span></p>",
+        "<p>\"When you realize nothing is lacking, the whole world belongs to you.\"<span>Lao Tzu</span></p>",
+        "<p>\"Zen is a path of liberation. It liberates you. It is freedom from the first step to the last. You are not required to follow any rules; you are required to find out your own rules and your own life in the light of awareness.\"<span>Osho</span></p>",
+        "<p>\"Treat every moment as your last. It is not preparation for something else.\"<span>Shunryu Suzuki</span></p>",
+        "<p>\"Nature does not hurry, yet everything is accomplished.\"<span>Lao Tzu</span></p>",
+        "<p>\"Be kind whenever possible. It is always possible.\"<span>Dalai Lama</span></p>",
+        "<p>\"Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.\"<span>Buddha</span></p>",
+        "<p>\"To seek is to suffer. To seek nothing is bliss.\"<span>Bodhidharma</span></p>",
+        "<p>\"All things are difficult before they are easy.\"<span>Thomas Fuller</span></p>",
+        "<p>\"Act without expectation.\"<span>Lao Tzu</span></p>",
+        "<p>\"Roads were made for journeys not destinations.\"<span>Confucius</span></p>",
+        "<p>\"Sleep is the best meditation.\"<span>Dalai Lama</span></p>",
+        "<p>\"The personal life deeply lived always expands into truths beyond itself.\"<span>Anais Nin</span></p>",,
+        "<p>\"It is easy to believe we are each waves and forget we are also the ocean.\"<span>Jon J. Muth</span></p>",
+        "<p>\"In the midst of chaos, there is also opportunity.\"<span>Sun Tzu</span></p>",
+        "<p>\"You look at where you’re going and where you are and it never makes sense, but then you look back at where you’ve been and a pattern seems to emerge.\" <span>Robert M. Pirsig</span></p>",
+        "<p>\"There is no need for temples; no need for complicated philosophy. Our own brain, our own heart is our temple; the philosophy is kindness.\"<span>Dalai Lama</span></p>",
+        "<p>\"As a bee gathering nectar does not harm or disturb the color & fragrance of the flower; so do the wise move through the world.\" <span>Buddha</span></p>",
+        "<p>\"The whole moon and the entire sky are reflected in one dewdrop on the grass.\"<span> Dogen</span></p>",
+        "<p>\"If your compassion does not include yourself, it is incomplete.\"<span>Buddha</span></p>",
+        "<p>\"They call it ‘peace of mind’ but maybe it should be called ‘peace from mind.\"<span>Naval Ravikant</span></p>",
+        "<p>\"If you think you are too small to make a difference, try sleeping with a mosquito.\" - Dalai Lama</p>",
+        "<p>\"A man is great not because he hasn’t failed; a man is great because failure hasn’t stopped him.\" <span>Confucius</span></p>",
+        "<p>\"Unhappiness and rain just drift by and are gone, they are visitors.\" <span>John Aske</span></p>",
+        "<p>\"When you catch yourself slipping into a pool of negativity, notice how it derives from nothing other than resistance to the current situation.\" <span>Donna Quesada</span></p>"   
     ],
     backgrounds: [
         { bgSource: "<img src='./images/sarah-dorweiler-357715-unsplash.jpg' alt='RANDOM ALT1 '>"},
@@ -52,19 +52,51 @@ app.data = {
         { bgSource: "<img src='./images/masaaki-komori-603921-unsplash.jpg' alt='RANDOM ALT TEXT 4'>"},
         { bgSource: "<img src='./images/quino-al-103766-unsplash.jpg' alt='RANDOM ALT TEXT 5'>"},
         { bgSource: "<img src='./images/cristina-gottardi-564469-unsplash.jpg' alt='RANDOM ALT TEXT 6'>"},
-        { bgSource: "<img src='./images/igor-son-320878-unsplash.jpg'' alt='RANDOM ALT TEXT 7'>"},
+        { bgSource: "<img src='./images/igor-son-320878-unsplash.jpg' alt='RANDOM ALT TEXT 7'>"},
         { bgSource: "<img src='./images/darran-shen-589140-unsplash.jpg' alt='RANDOM ALT TEXT 8'>"},
         { bgSource: "<img src='./images/kara-eads-1080482-unsplash.jpg' alt='RANDOM ALT TEXT 9'>"},
-        { bgSource: "<img src='./images/max-di-capua-1064162-unsplash.jpg' alt='RANDOM ALT TEXT 10'>"},
-        { bgSource: "<img src='./images/elin-tabitha-1092029-unsplash.jpg' alt='RANDOM ALT TEXT 11'>"},
-        { bgSource: "<img src='./images/jeremy-bishop-338661-unsplash.jpg' alt='RANDOM ALT TEXT 12'>"}
+        // { bgSource: "<img src='./images/max-di-capua-1064162-unsplash.jpg' alt='RANDOM ALT TEXT 10'>"},
+        // { bgSource: "<img src='./images/elin-tabitha-1092029-unsplash.jpg' alt='RANDOM ALT TEXT 11'>"},
+        // { bgSource: "<img src='./images/jeremy-bishop-338661-unsplash.jpg' alt='RANDOM ALT TEXT 12'>"}
+        { bgSource: "<img src='./images/bench-accounting-49909-unsplash.jpg' alt='RANDOM ALT TEXT 11'>" },
+        { bgSource: "<img src='./images/carolinie-cavalli-99609-unsplash.jpg' alt='RANDOM ALT TEXT 12'>"},
+        { bgSource: "<img src='./images/chrissie-kremer-566235-unsplash.jpg' alt='RANDOM ALT TEXT 13'>" },
+        { bgSource: "<img src='./images/danielle-macinnes-51592-unsplash.jpg' alt='RANDOM ALT TEXT 14'>" },
+        { bgSource: "<img src='./images/davies-designs-379022-unsplash.jpg' alt='RANDOM ALT TEXT 15'>" },
+        { bgSource: "<img src='./images/dominik-schroder-14534-unsplash.jpg' alt='RANDOM ALT TEXT 16'>" },
+        { bgSource: "<img src='./images/galina-n-205299-unsplash.jpg' alt='RANDOM ALT TEXT 17'>" },
+        { bgSource: "<img src='./images/joel-peel-131757-unsplash.jpg' alt='RANDOM ALT TEXT 18'>" },
+        { bgSource: "<img src='./images/kumiko-shimizu-629602-unsplash.jpg' alt='RANDOM ALT TEXT 19'>" },
+        { bgSource: "<img src='./images/masaaki-komori-597700-unsplash.jpg' alt='RANDOM ALT TEXT 20'>" },
+        { bgSource: "<img src='./images/masaaki-komori-654069-unsplash.jpg' alt='RANDOM ALT TEXT 21'>" },
+        { bgSource: "<img src='./images/nordwood-themes-490552-unsplash.jpg' alt='RANDOM ALT TEXT 22'>" },
+        { bgSource: "<img src='./images/paul-earle-39322-unsplash.jpg' alt='RANDOM ALT TEXT 23'>" },
+        { bgSource: "<img src='./images/thomas-kinto-1057481-unsplash.jpg' alt='RANDOM ALT TEXT 24'>" },
+        { bgSource: "<img src='./images/vitalis-hirschmann-504920-unsplash.jpg' alt='RANDOM ALT TEXT 25'>" }
+
     ]
 }
+// { bgSource: "<img src='./images/chrissie-kremer-566235-unsplash.jpg' alt='RANDOM ALT TEXT'>"},
+// { bgSource: "<img src='./images/danielle-macinnes-51592-unsplash.jpg' alt='RANDOM ALT TEXT'>"},
+// { bgSource: "<img src='./images/davies-designs-379022-unsplash.jpg' alt='RANDOM ALT TEXT'>"},
+// { bgSource: "<img src='./images/dominik-schroder-14534-unsplash.jpg' alt='RANDOM ALT TEXT'>"},
+// { bgSource: "<img src='./images/galina-n-205299-unsplash.jpg' alt='RANDOM ALT TEXT'>"},
+// { bgSource: "<img src='./images/joel-peel-131757-unsplash.jpg' alt='RANDOM ALT TEXT'>"},
+// { bgSource: "<img src='./images/kumiko-shimizu-629602-unsplash.jpg' alt='RANDOM ALT TEXT'>"},
+// { bgSource: "<img src='./images/masaaki-komori-597700-unsplash.jpg' alt='RANDOM ALT TEXT'>"},
+// { bgSource: "<img src='./images/masaaki-komori-654069-unsplash.jpg' alt='RANDOM ALT TEXT'>"},
+// { bgSource: "<img src='./images/nordwood-themes-490552-unsplash.jpg' alt='RANDOM ALT TEXT'>"},
+// { bgSource: "<img src='./images/paul-earle-39322-unsplash.jpg' alt='RANDOM ALT TEXT'>"},
+// { bgSource: "<img src='./images/thomas-kinto-1057481-unsplash.jpg' alt='RANDOM ALT TEXT'>"},
+// { bgSource: "<img src='./images/vitalis-hirschmann-504920-unsplash.jpg' alt='RANDOM ALT TEXT'>"}
+
+
+
 
 app.init = function() {
     // PAGE LOAD FADE IN....
     $('main').css('display', 'none');
-    $('main').fadeIn(1000);
+    $('main').fadeIn(2000);
 
     // ON BUTTON CLICK, execute random quote and bg functions, IF not locked... 
     $("button").on("click", function() {
@@ -73,7 +105,7 @@ app.init = function() {
         if (app.quoteLocked === false) {
         $(".quote-result").fadeOut(1200, function() {
             app.getRandomQuote();
-            $(".quote-result").fadeIn(1200)
+            $(".quote-result").fadeIn(2000)
         })}
 
         
@@ -88,13 +120,18 @@ app.init = function() {
 
 
         if (app.bgLocked === false) {
-            $(".background-image").fadeOut(1200, function() {
+            $(".background-image").fadeOut(500, function() {
+                console.log("fadeout");
                 app.getRandomBackground();
-                $(".background-image").fadeIn(1200);
+                setTimeout( function() {
+                    $(".background-image").fadeIn(500);
+                }, 500);
+                // $(".background-image").fadeIn(1200);
+                // console.log("fadin");
             })
         }
     }); // END ON CLICK function
-} // END INIT FUNCTION  ****************
+} // END INIT FUNCTION  ***********f*****
 
 // if quote not locked, retreive random quote, insert into quote-wrapper div
 app.quoteLocked = false;
